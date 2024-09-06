a6af470f1a540776c0287fbba7ad7fa9712ae831
Rajat Sangrame <rajat.sangrame@gmail.com>
2024-09-07 02:49:10 +0530

    added auth moduel

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    const user = await this.authService.validateUser(
+      loginDto.username,
+      loginDto.password,
+    );
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return this.authService.login(user);
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = { userId: 1, username: 'rajat', password: 'rajat189' };
+    if (user.password === password) {
+      return user;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
+}
